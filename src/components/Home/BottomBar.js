6c11cf51c9864c9dfc9a1d28c3b762352a7f1dc2
Phan Tung <tungpt.seven@gmail.com>
2020-07-08 16:25:16 +0700

    change bottom bar + done News page UI

--- src/components/Home/BottomBar.js
+++ src/components/Home/BottomBar.js
@@ -4,17 +4,15 @@ import Feather from 'react-native-vector-icons/Feather'
 
 const BottomBar = () => {
   return (
-    <View>
+    <View style={styles.main}>
       <View style={styles.container}>
-        <View style={[styles.col1, { borderTopRightRadius: 200 }]}>
+        <View style={[styles.col1]}>
           <Feather name='cast' size={30} color='#B6BBC4'></Feather>
         </View>
-        <View style={[styles.col1, { borderTopLeftRadius: 200 }]}>
+        <View style={styles.col1}>
           <Feather name='bell' size={30} color='#B6BBC4'></Feather>
         </View>
       </View>
-      <View style={styles.center}></View>
-      <View style={styles.center2}></View>
       <View style={styles.volumnCon}>
         <Feather name='mic' size={30} color='#222' />
       </View>
@@ -25,13 +23,20 @@ const BottomBar = () => {
 export default BottomBar
 
 const styles = StyleSheet.create({
+  main: {
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    bottom: 25,
+  },
   container: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    backgroundColor: '#F4F4F4'
+    backgroundColor: 'transparent',
+    // '#F4F4F4'
   },
   col1: {
-    width: '42%',
+    width: '50%',
+    // borderTopRightRadius: 200,
     backgroundColor: '#fff',
     padding: 10,
     alignItems: 'center'
@@ -48,7 +53,9 @@ const styles = StyleSheet.create({
   center2: {
     alignSelf: 'center',
     position: 'absolute',
-    backgroundColor: '#F4F4F4',
+    backgroundColor: 'green',
+    opacity: 0.1,
+    // '#F4F4F4',
     width: 90,
     height: 90,
     top: -56,
@@ -60,7 +67,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     width: 90,
     height: 90,
-    top: -80,
+    top: -50,
     borderRadius: 100,
     justifyContent: 'center',
     alignItems: 'center',
--- src/screens/NewsScreen.js
+++ src/screens/NewsScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image, Dimensions, ImageBackground } from 'react-native'
+import { View, Text, StyleSheet, Image, Dimensions, ImageBackground, ScrollView } from 'react-native'
+import Entypo from 'react-native-vector-icons/Entypo'
 import LinearGradient from 'react-native-linear-gradient'
 
 const window = Dimensions.get("window")
@@ -7,27 +8,54 @@ const window = Dimensions.get("window")
 const NewsScreen = () => {
   return (
     <View style={styles.flex}>
-      <View style={styles.highLight}>
+      <Entypo name='menu' size={30} style={styles.menu}></Entypo>
+      <View style={styles.highLightContainer}>
         <ImageBackground source={require('../resources/news1.png')} style={styles.image}>
           <LinearGradient colors={["transparent", "black"]} locations={[0.6, 1]} style={styles.linearGradient}>
-            <Text style={styles.buttonText}>
-              Sign in with Facebook
-          </Text>
+            <View style={styles.hightLightButton}>
+              <Text style={{ color: '#fff' }}>
+                Hightlights
+            </Text>
+            </View>
+            <Text style={styles.hightLightTitle}>
+              German Chancellor Angela Merkel goes into self-quarantine
+            </Text>
           </LinearGradient>
         </ImageBackground>
-        {/* <View style={styles.title}>
-          <Text>
-            Hightlights
-          </Text>
-          <Text>
-            German Chancellor Angela Merkel goes into self-quarantine
-          </Text>
-        </View> */}
       </View>
-      <View style={styles.related}>
-
+      <View style={styles.relatedContainer}>
+        <Text style={styles.relatedHeader}>Related News</Text>
+        <ScrollView>
+          <View style={styles.relatedNew}>
+            <Image source={require('../resources/news2.png')} style={styles.relatedImage} />
+            <View style={styles.newInfo}>
+              <Text style={styles.time}>12 hr 26 min ago</Text>
+              <Text style={styles.relatedTitle}>DC to use National Guard to help prevent cherry blossom crowds in nation’s capital</Text>
+            </View>
+          </View>
+          <View style={styles.relatedNew}>
+            <Image source={require('../resources/news2.png')} style={styles.relatedImage} />
+            <View style={styles.newInfo}>
+              <Text style={styles.time}>12 hr 26 min ago</Text>
+              <Text style={styles.relatedTitle}>DC to use National Guard to help prevent cherry blossom crowds in nation’s capital</Text>
+            </View>
+          </View>
+          <View style={styles.relatedNew}>
+            <Image source={require('../resources/news2.png')} style={styles.relatedImage} />
+            <View style={styles.newInfo}>
+              <Text style={styles.time}>12 hr 26 min ago</Text>
+              <Text style={styles.relatedTitle}>DC to use National Guard to help prevent cherry blossom crowds in nation’s capital</Text>
+            </View>
+          </View>
+          <View style={styles.relatedNew}>
+            <Image source={require('../resources/news2.png')} style={styles.relatedImage} />
+            <View style={styles.newInfo}>
+              <Text style={styles.time}>12 hr 26 min ago</Text>
+              <Text style={styles.relatedTitle}>DC to use National Guard to help prevent cherry blossom crowds in nation’s capital</Text>
+            </View>
+          </View>
+        </ScrollView>
       </View>
-      <Text></Text>
     </View>
   )
 }
@@ -39,7 +67,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F4F4F4',
   },
-  highLight: {
+  menu: {
+    color: '#fff',
+    zIndex: 100,
+    position: 'absolute',
+    left: 20,
+    top: 20
+  },
+  highLightContainer: {
     height: window.height * 0.45,
     overflow: 'hidden',
   },
@@ -52,7 +87,50 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     justifyContent: "center"
   },
-  related: {},
+  hightLightButton: {
+    fontSize: 16,
+    // fontFamily: 'Gill Sans',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    backgroundColor: '#FA5252',
+    width: 89,
+    height: 28
+  },
+  hightLightTitle: {
+    fontSize: 30,
+    fontWeight: '700',
+    color: '#fff',
+    paddingHorizontal: 20,
+  },
+  relatedContainer: {
+    flex: 1,
+    backgroundColor: '#F4F4F4',
+    paddingHorizontal: 20,
+    paddingTop: 27
+    // '#F2F6F8'
+  },
+  relatedHeader: {
+    fontSize: 17,
+    marginBottom: 15
+  },
+  relatedNew: {
+    flexDirection: 'row',
+    paddingHorizontal: 5,
+    marginBottom: 18
+  },
+  newInfo: {
+    flex: 1,
+    marginLeft: 10,
+    paddingTop: 1,
+  },
+  time: {
+    fontSize: 15,
+    color: '#AFB8BE',
+  },
+  relatedTitle: {
+    fontSize: 16
+  },
   linearGradient: {
     flex: 1,
     backgroundColor: "transparent",
@@ -60,7 +138,10 @@ const styles = StyleSheet.create({
     top: 0,
     bottom: 0,
     left: 0,
-    right: 0
+    right: 0,
+    paddingBottom: 27,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
   },
   buttonText: {
     fontSize: 18,
